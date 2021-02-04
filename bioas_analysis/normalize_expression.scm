31f7e0f02a3e541a919f2db1ccfe69a6e56079c0
Hedra <tanksh24@gmail.com>
2021-02-04 13:34:27 -0500

    Add path argument

--- bioas_analysis/normalize_expression.scm
+++ bioas_analysis/normalize_expression.scm
@@ -1,35 +1,36 @@
 (use-modules (pln-bio expr) (opencog logger) (pln-bio bio-utils)) 
 ;; install https://github.com/Habush/pln-brca-xp.git for these modules
 
-(define (run-expr-deduction overexpr?)
+(define (run-expr-deduction overexpr? path)
     (cog-logger-set-stdout! #t)
     (if overexpr?
         (begin 
             (cog-logger-info "Loading patient overexpression data")
             ;;load the atomese form of overexpr & underexpr
-            (primitive-load "cancer_data/patient_gene_over_expr.scm")        
+            (primitive-load (string-append path "/patient_gene_over_expr.scm"))      
             ;;generate the quantiles for overexpr
             (cog-logger-info "Generating SchemaValueLists")
             (overexpression-dist)
             ;;get the evaluation links for overexpr
             (cog-logger-info "Generating EvaluationLinks")
             (get-overexpr-eval-ln)
-            (write-atoms-to-file "cancer_data/normalized_patient_gene_over_expr.scm" (cog-get-atoms 'EvaluationLink))
+            (write-atoms-to-file (string-append path "/normalized_patient_gene_over_expr.scm") (cog-get-atoms 'EvaluationLink))
             (cog-logger-info "Done!")
         )
         (begin 
             (cog-logger-info "Loading patient underexpression data")
             ;;load the atomese form of overexpr & underexpr
-            (primitive-load "cancer_data/patient_gene_under_expr.scm")
+            (primitive-load (string-append path "/patient_gene_under_expr.scm"))
             ;;generate the quantiles for overexpr
             (cog-logger-info "Generating SchemaValueLists")
             (underexpression-dist)
             ;;get the evaluation links for overexpr
             (cog-logger-info "Generating EvaluationLinks")
             (get-underexpr-eval-ln)
-            (write-atoms-to-file "cancer_data/normalized_patient_gene_under_expr.scm" (cog-get-atoms 'EvaluationLink))
+            (write-atoms-to-file (string-append path "/normalized_patient_gene_under_expr.scm") (cog-get-atoms 'EvaluationLink))
             (cog-logger-info "Done!"))))
 
-(run-expr-deduction #t)
+(define path "/home/hedra/SIngularityNEt/knowledge-import/cancer/moses_ft83")
+(run-expr-deduction #t path)
 (clear)
-(run-expr-deduction #f)
+(run-expr-deduction #f path)
