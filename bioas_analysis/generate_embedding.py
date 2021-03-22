f9c61bed760f5685a551b60694b8623be684d980
Hedra Seid <tanksh24@gmail.com>
2021-03-22 17:33:32 +0100

    Updated for the latest changes

--- bioas_analysis/generate_embedding.py
+++ bioas_analysis/generate_embedding.py
@@ -9,37 +9,54 @@ from matplotlib import pyplot
 from scipy.spatial import distance
 from scipy.stats import kendalltau, pearsonr, spearmanr
 from sklearn.decomposition import PCA, KernelPCA
+from sklearn.preprocessing import StandardScaler, MinMaxScaler, QuantileTransformer
 from datetime import date
 from utils import *
 import pandas as pd
 
 zerovector = []
 
-def generate_embeddings(embedding_method, data_dir, node_type, kb_atomspace=False, p=1.5, T=2,normalization=False):
-    if embedding_method == "FMBPV":
-      if kb_atomspace:
-        atomspace = kb_atomspace
-      else:
-        print("--- Load the AtomSpace")
-        atomspace = AtomSpace()
-        initialize_opencog(atomspace)
-        scheme_eval(atomspace, "(use-modules (opencog persist-file))")
-        for i in os.listdir(data_dir):
-            if i.endswith(".scm"):
-                scheme_eval(atomspace,"(load-file \"{}/{}\")".format(data_dir, i))
+def generate_embeddings(data_dir, node_type, kb_atomspace=False, p=1, T=2,normalization=False,
+                        test_data=False, vector_space=False):
+  if kb_atomspace:
+    atomspace = kb_atomspace
+  else:
+    print("--- Loading the AtomSpace")
+    atomspace = AtomSpace()
+    initialize_opencog(atomspace)
+    scheme_eval(atomspace, "(use-modules (opencog persist-file))")
+    for i in os.listdir(data_dir):
+        if i.endswith(".scm"):
+            scheme_eval(atomspace,"(load-file \"{}/{}\")".format(data_dir, i))
 
-      property_vectors = build_property_vectors(atomspace, data_dir, node_type, p, T, normalization=normalization)
-      property_vectors = do_kpca(property_vectors)
-      result = export_property_vectors(data_dir, property_vectors,p,T, normalization)
-      return result
+  property_vectors = build_property_vectors(atomspace, data_dir, node_type, p, T, normalization=normalization, 
+                                            test_data=test_data, vector_space=vector_space)
+  if test_data:
+    # Patients in the test or training might not have all attributes, fill missing ones with 0
+    if len(property_vectors.columns) < len(vector_space.columns):
+      rows = property_vectors.shape[0]
+      col_diff = len(vector_space.columns) - len(property_vectors.columns)
+      for i in range(col_diff):
+        property_vectors["missign{}".format(i)] = [0]*rows
+    elif len(property_vectors.columns) > len(vector_space.columns):
+      rows = vector_space.shape[0]
+      col_diff = len(property_vectors.columns) - len(vector_space.columns)
+      for i in range(col_diff):
+        vector_space["missign{}".format(i)] = [0]*rows
+    embedding_vector = do_kpca(property_vectors, test_data=test_data, vector_space=vector_space)
+  else:    
+    embedding_vector = do_kpca(property_vectors)
 
-def build_property_vectors(atomspace, data_dir, node_type, p, T, normalization=False):
+  result = export_property_vectors(data_dir, embedding_vector,p,T, normalization)
+  return result
+
+def build_property_vectors(atomspace, data_dir, node_type, p, T, normalization=False,test_data=False, vector_space=False):
   print("--- Building property vectors")
   ppty = set([i.out[1] for i in atomspace.get_atoms_by_type(types.AttractionLink) if i.out[1].type_name != "VariableNode"])
   nodes = set([i.out[0] for i in atomspace.get_atoms_by_type(types.AttractionLink)])
-  index_mapping = dict(zip(range(len(ppty)), ppty))
+  ppty_mapping = dict(zip(range(len(ppty)), [str(i) for i in ppty]))
 
-  property_df = pd.DataFrame([], columns=["patient_ID"] + list(index_mapping.keys()))
+  property_df = pd.DataFrame([], columns=["patient_ID"] + list(ppty_mapping.keys()))
   print("Number of {}: {}".format(node_type, len(atomspace.get_atoms_by_type(getattr(types, node_type)))))
   print("Number of properties: {}".format(len(ppty)))
   for node in nodes:
@@ -50,7 +67,7 @@ def build_property_vectors(atomspace, data_dir, node_type, p, T, normalization=F
               attraction = AttractionLink(node, pt)
               attraction_tv = attraction.tv
               # weighted product, to give more weight to the strength s^p * c^(T-p)
-              # with default p = 1.5 and T = 2
+              # with default p = 1 and T = 2
               wp = attraction.tv.mean**p * attraction.tv.confidence**(T-p)
               p_vec.append(wp)
           else:
@@ -65,72 +82,99 @@ def build_property_vectors(atomspace, data_dir, node_type, p, T, normalization=F
 
   # normalize the vector
   if normalization:
-    if normalization == "scaling":
-      for column_name in index_mapping.keys():
-        property_df[column_name] = property_df[column_name] / max(property_df[column_name])
-    elif normalization == "standard":
-      for column_name in index_mapping.keys():
-        column_mean = property_df[column_name].mean()
-        column_std = property_df[column_name].std()
-        if column_std == 0:
-          property_df = property_df.drop([column_name], axis=1)
-        else:
-          property_df[column_name] = (property_df[column_name] - column_mean) / column_std
+    if test_data:
+        property_df = normalize_df(property_df, normalization,test_data=True, train_data=vector_space)
     else:
-      property_df = quantile_normalize(property_df)
+        property_df = normalize_df(property_df, normalization)
 
   # Dump the property vector to CSV (before applying kpca)
-  if not normalization:
-    normalization = "notnormalized"
   property_vector_pickle_beforekpca = os.path.join(data_dir, 
-            "property_vector_beforekpca_p={},T={}_{}_{}.csv".format(p, T, normalization,str(date.today())))
+            "property_vector_beforekpca_p={},T={}_{}_{}.csv".format(p, T, normalization if normalization else "notnormalized",str(date.today())))
   property_df.to_csv(property_vector_pickle_beforekpca, sep="\t", index=False)
-
+  filehandler = open(os.path.join(data_dir, "ppty_mapping_{}".format(normalization if normalization else "notnormalized")), 'wb')
+  pickle.dump(ppty_mapping, filehandler)
   return property_df
 
-def do_kpca(property_vectors_df):
-  def kernel_func(X):
+def normalize_df(property_df, normalization, test_data=False, train_data=False):
+  cols = property_df.columns.drop("patient_ID")
+  if test_data:
+    train_data = train_data.drop("patient_ID")
+  if normalization == "scaling":
+    scaler = MinMaxScaler()
+    if test_data:
+      scaler.fit(train_data)
+      property_df[cols] = scaler.transform(property_df[cols])
+    else:
+      property_df[cols] = scaler.fit_transform(property_df[cols])
+  elif normalization == "standard":
+    scaler = StandardScaler()
+    if test_data:
+      scaler.fit(train_data)
+      property_df[cols] = scaler.transform(property_df[cols])
+    else:
+      property_df[cols] = scaler.fit_transform(property_df[cols])
+  else:
+    qt = QuantileTransformer(n_quantiles=5, random_state=0, output_distribution="normal")
+    if test_data:
+      qt.fit(train_data)
+      property_df[cols] = qt.transform(property_df[cols])
+    else:
+      property_df[cols] = qt.fit_transform(property_df[cols])
+  return property_df
+
+def do_kpca(property_vectors_df, test_data=False, vector_space=False, n_components=150, tan_dist=False):
+  def kernel_func(X, Y):
+    X = sparse.vstack(X.values())
+    Y = sparse.vstack(Y.values())
     len_x = X.shape[0]
-    dist_array = numpy.random.random((len_x, len_x)).astype(numpy.float16) * 0 - 1.0
-    mat = []
+    len_y = Y.shape[0]
+    dist_array = numpy.random.random((len_x, len_y)).astype(numpy.float16) * 0 - 1.0
     i = 0
-    cnt = 0
-    total = len_x ** 2
     for a in X:
       a = a.toarray().flatten()
-      row = []
       j = 0
-      print("--- Working on {}/{} ...".format(cnt, total))
-      for b in X:
+      for b in Y:
         b = b.toarray().flatten()
-        cnt += 1
-        if 0 <= dist_array[i, j]:
-          row.append(dist_array[i, j])
-        elif 0 <= dist_array[j, i]:
-          row.append(dist_array[j, i])
+        if tan_dist:
+          dist = tanimoto_kernel(a, b)
         else:
-          # dist = fuzzy_jaccard(a, b)
           dist = tanimoto(a, b)
-          row.append(dist)
-          dist_array[i, j] = dist
+        dist_array[i, j] = dist
         j += 1
-      mat.append(numpy.array(row).astype(numpy.float16))
       i += 1
-    return numpy.array(mat, dtype=numpy.float16)
+    return dist_array
 
   print("--- Doing KPCA")
+  kpca = KernelPCA(kernel = "precomputed", n_components=n_components)
+  property_vectors_dict = compress_df(property_vectors_df)
+  if test_data:
+    print("Train data:{}, Test data:{}".format(vector_space.shape, property_vectors_df.shape))
+    train_data = compress_df(vector_space)
+    train_space = kernel_func(train_data, train_data)
+    # Fit with train data and do just transform on test
+    kpca.fit(train_space)
+    test_space = kernel_func(property_vectors_dict, train_data)
+    print("--- Test data : {}".format(test_space.shape))
+    result_kpca = kpca.transform(test_space)
 
-  # Compress the vector dataframe to dict
-  property_vectors_dict = {}
-  for i,j in enumerate(property_vectors_df["patient_ID"]):
-    property_vectors_dict[j] = sparse.csr_matrix(property_vectors_df.loc[i,property_vectors_df.columns.drop("patient_ID")].values.tolist())
-
-  X = sparse.vstack(property_vectors_dict.values())
-  X_kpca = KernelPCA(kernel = "precomputed").fit_transform(kernel_func(X))
-
-  for k, kpca_v in zip(property_vectors_dict.keys(), X_kpca):
+  else:
+    result_kpca = kpca.fit_transform(kernel_func(property_vectors_dict, property_vectors_dict))
+ 
+  for k, kpca_v in zip(property_vectors_dict.keys(), result_kpca):
     property_vectors_dict[k] = kpca_v
-  return property_vectors_dict
+  return dict_to_df(property_vectors_dict)
+
+def dict_to_df(res_dict):
+  df = pd.DataFrame(res_dict.values())
+  df["patient_ID"] = res_dict.keys()
+  return df
+
+def compress_df(df):
+  # Compress the dataframe (sparse matrix) to dict
+  vectors_dict = {}
+  for i,j in enumerate(df["patient_ID"]):
+    vectors_dict[j] = sparse.csr_matrix(df.loc[i,df.columns.drop("patient_ID")].values.tolist())
+  return vectors_dict
 
 def export_property_vectors(data_dir, property_vectors,p,T, normalization):
   if not normalization:
@@ -140,17 +184,25 @@ def export_property_vectors(data_dir, property_vectors,p,T, normalization):
 
   print("--- Exporting property vectors to \"{}\"".format(output_file))
 
-  df = dict_to_csv(property_vectors)
-  df.to_csv(output_file, sep="\t", index=False)
+  property_vectors.to_csv(output_file, sep="\t", index=False)
 
   if len(zerovector) > 0:
     print(len(zerovector))
     with open(os.path.join(data_dir , "zerovector.txt"), "w") as z:
       z.write("\n".join(zerovector))
-  return df
+  return property_vectors
 
 def tanimoto(v1, v2):
   v1_v2 = numpy.dot(v1, v2)
   v1_sq = numpy.sum(numpy.square(v1))
   v2_sq = numpy.sum(numpy.square(v2))
   return v1_v2 / (v1_sq + v2_sq - v1_v2)
+
+def tanimoto_kernel(v1, v2):
+    v1_norm = numpy.linalg.norm(v1, ord=1)
+    v2_norm = numpy.linalg.norm(v2, ord=1)
+    v1_v2_norm = numpy.linalg.norm((v1 - v2), ord=1)
+    f = (v1_norm + v2_norm - v1_v2_norm)
+    g = (v1_norm + v2_norm + v1_v2_norm)
+    result = f / g
+    return result
--- bioas_analysis/patients_analysis.py
+++ bioas_analysis/patients_analysis.py
@@ -18,22 +18,8 @@ def preprocess(atomspace, universe):
     if e.tv.mean == 0:
       scheme_eval(atomspace,"(cog-delete {})".format(e))
 
-  # remove patients with no gene expression
-  filter_out = []
-  all_patients = atomspace.get_atoms_by_type(types.PatientNode)
-  with_geneexpression = set([x.out[1] for x in atomspace.get_atoms_by_type(types.EvaluationLink) if x.out[0].type == types.LazyExecutionOutputLink])
-  for p in all_patients:
-    if universe:
-      # remove patients not in the given universe set
-      if not p.name in universe:
-        scheme_eval(atomspace, "(cog-delete-recursive! {})".format(p))
-    if p not in with_geneexpression:
-      filter_out.append(str(p))
-      scheme_eval(atomspace, "(cog-delete-recursive! {})".format(p))
-  with open(base_results_dir + "filtered_out_patients.scm", "w") as f:
-    f.write("\n".join(filter_out))
-  
-  return len(with_geneexpression)
+  all_patients = atomspace.get_atoms_by_type(types.PatientNode)  
+  return len(all_patients)
 
 def apply_subset_rule1(atomspace):
   print("--- {} Inferring subsets, rule 1".format(datetime.now()))
@@ -119,7 +105,7 @@ def remove_processed_subsets(atomspace):
 
 def export_all_atoms(atomspace, base_results_dir):
   print("--- {} Exporting Atoms to files".format(datetime.now()))
-  result_scm = base_results_dir + "AttractionLinks-results.scm"
+  result_scm = base_results_dir + "AttractionLinks-results.scm.bc"
   att = atomspace.get_atoms_by_type(types.AttractionLink)
   with open(result_scm, "w") as f:
     f.write("\n".join([str(a) for a in att]))
@@ -164,6 +150,8 @@ def parse_args():
                         help='The value for p in the weight product s^p * c^(T-p)')
     parser.add_argument('--T', type=str, default='',
                         help='The value for T in the weight product s^p * c^(T-p)')
+    parser.add_argument('--vec_space', type=str, default='',
+                        help='The initial vector space to use as a base')
     return parser.parse_args()
 
 if __name__ == "__main__":
@@ -191,15 +179,26 @@ if __name__ == "__main__":
   if arguments.p:
     p = float(arguments.p)
   else:
-    p = 1.5 
+    p = 1
   if arguments.T:
     T = float(arguments.T)
   else:
     T = 2
+  if arguments.vec_space:
+    vec_space = pd.read_csv(arguments.vec_space, sep="\t")
+    test_dataset = True
+  else:
+    test_dataset = False
 
   if not os.path.exists(base_results_dir):
-    os.makedirs(base_results_dir) 
+    os.makedirs(base_results_dir)
+
   kb_as = generate_atoms(base_results_dir, base_datasets_dir, filterbp, universe=universe_patients)
-  generate_embeddings("FMBPV",base_results_dir,"PatientNode", kb_atomspace=kb_as, p=p, T=T, normalization=norm)
-  export_all_atoms(kb_as, base_results_dir)
+  export_all_atoms(kb_as, base_results_dir) 
+
+  if test_dataset:
+    generate_embeddings(base_results_dir,"PatientNode", kb_atomspace=kb_as, p=p, T=T, normalization=norm, 
+                        test_data=True, vector_space=vec_space)  
+  else:
+    generate_embeddings(base_results_dir,"PatientNode", kb_atomspace=kb_as, p=p, T=T, normalization=norm)
   print("Done {}".format(datetime.now()))
