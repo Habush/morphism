e1288e9a546b1dd6c7a6da1a4e5d93b1715dc6c5
Hedra <tanksh24@gmail.com>
2021-04-30 12:32:19 -0400

    Additional rule for gene property

--- bioas_analysis/genes_analysis.py
+++ bioas_analysis/genes_analysis.py
@@ -1,14 +1,16 @@
 import os
 from utils import *
+from generate_embedding import *
+from preprocess import calculate_truth_values, infer_subsets
+import argparse
 
 log.set_level("ERROR")
 
 def load_atomspace(datapath, atomspace):
   print("--- Load preprocessed data to the AtomSpace")
-  scheme_eval(atomspace, "(load-file \"{}/{}\")".format(datapath, "member-links.scm"))  
-  scheme_eval(atomspace, "(load-file \"{}/{}\")".format(datapath, "inheritance-links.scm"))
-  # subset links are between GO catagories and we dont need them here 
-  # scheme_eval(atomspace, "(load-file \"{}/{}\")".format(datapath, "subset-links.scm")) 
+  for i in os.listdir(datapath):
+    if i.endswith(".scm"):
+      scheme_eval(atomspace, "(load-file \"{}\")".format(os.path.join(datapath, i)))  
 
 def generate_subsets(atomspace):
   print("--- Infer subsets between gene singletons and concepts")
@@ -35,56 +37,106 @@ def gene_singleton_tv(atomspace):
 
 def infer_negation(atomspace):
   print("--- Inferring Subset Negation")
+  # (Subset (Set A) B) |- (Subset (Not (Set A) B))
+  target = """
+    (Subset
+      (Not (Set (Variable "$X")))
+        (Variable "$Y"))"""
+  bc = """
+    (pln-bc 
+      {}
+      #:maximum-iterations 2
+      #:complexity-penalty 10)"""
   scheme_eval(atomspace, "(pln-load 'empty)")
   scheme_eval(atomspace, "(pln-load-from-path \"rules/subset_negation_rule.scm\")")
-  # (Subset (Set A) B) |- (Subset (Not (Set A) B))
   scheme_eval(atomspace, "(pln-add-rule \"subset-condition-negation-genes-rule\")")
-  scheme_eval(atomspace, " ".join(["(pln-bc",
-                  "(Subset (Not (Set (Variable \"$X\"))) (Variable \"$Y\"))",
-                  "#:vardecl",
-                    "(VariableSet",
-                      "(TypedVariable (Variable \"$X\") (Type \"GeneNode\"))",
-                      "(TypedVariable (Variable \"$Y\") (TypeInh \"ConceptNode\")))",
-                  "#:maximum-iterations 12",
-                  "#:complexity-penalty 10)"]))
+  scheme_eval(atomspace, bc.format(target))
 
 def infer_attractions(atomspace):
   print("--- Inferring AttractionLinks")
+  # (Subset (Set A) B)) (Subset (Not (Set A) B)) |- (Attraction (Set A) B)
+  target = """ 
+  (Attraction 
+      (Set (Variable "$X")) 
+      (Variable "$Y"))
+  """
+  bc = """
+  (pln-bc 
+    {}
+    #:maximum-iterations 2
+    #:complexity-penalty 10)"""
   scheme_eval(atomspace, "(pln-load 'empty)")
   scheme_eval(atomspace, "(pln-load-from-path \"rules/subset_attraction_rule.scm\")")
-  # (Subset (Set A) B)) (Subset (Not (Set A) B)) |- (Attraction (Set A) B)
   scheme_eval(atomspace, "(pln-add-rule \"subset-attraction-genes-rule\")")
-  scheme_eval(atomspace, " ".join(["(pln-bc",
-                  "(Attraction (Set (Variable \"$X\")) (Variable \"$Y\"))",
-                  "#:vardecl",
-                    "(VariableSet",
-                      "(TypedVariable (Variable \"$X\") (Type \"GeneNode\"))",
-                      "(TypedVariable (Variable \"$Y\") (TypeInh \"ConceptNode\")))",
-                  "#:maximum-iterations 12",
-                  "#:complexity-penalty 10)"]))
+  scheme_eval(atomspace, bc.format(target))
+
+def get_intentional_similarity(atomspace):
+  # (Attraction (Set A) X)) (Attraction (Set B) X)) |- Intensional-similarity (Set A) (Set B))
+  scheme_eval(atomspace, "(pln-load 'empty)")
+  scheme_eval(atomspace, "(pln-load-from-path \"rules/genes_intensional_similarity.scm\")")
+  scheme_eval(atomspace, "(pln-add-rule \"genes-intensional-similarity-rule\")")
+  target = """ 
+  (IntensionalSimilarity 
+      (Set (Variable "$X")) 
+      (Set (Variable "$Y")))
+  """
+  bc = """
+  (pln-bc 
+    {}
+    #:maximum-iterations 2
+    #:complexity-penalty 10)"""
+  scheme_eval(atomspace, bc.format(target))
 
 def export_result(datapath, atomspace):
-  attraction_links_scm = os.path.join(datapath, "attraction-links-genes.scm")
-  print("--- Exporting attraction links to files")
-  scheme_eval(atomspace, " ".join([
-  "(define (write-atoms-to-file file atoms)",
-    "(define fp (open-output-file file))",
-    "(for-each",
-      "(lambda (x) (display x fp))",
-      "atoms)",
-    "(close-port fp))"]))
-  write_atoms_to_file(attraction_links_scm, "(cog-get-atoms 'AttractionLink)", atomspace)
+  print("--- {} Exporting Atoms to files")
+  result_scm = os.path.join(datapath, "AttractionLinks-results.scm.bc")
+  att = atomspace.get_atoms_by_type(types.AttractionLink) + atomspace.get_atoms_by_type(types.IntensionalSimilarityLink)
+  with open(result_scm, "w") as f:
+    f.write("\n".join([str(a) for a in att]))
 
-def generate_attractionlinks_genes(datapath):
+def generate_links_genes(datapath, doinhrule=False):
   ### Initialize the AtomSpace ###
   atomspace = AtomSpace()
   initialize_opencog(atomspace)
 
   scheme_eval(atomspace, "(add-to-load-path \".\")")
-  scheme_eval(atomspace, "(use-modules (opencog) (opencog bioscience) (opencog ure) (opencog pln) (opencog persist-file) (srfi srfi-1))")
+  scheme_eval(atomspace, "(use-modules (opencog) (opencog exec) (opencog logger) (opencog bioscience) (opencog ure) (opencog pln) (opencog persist-file) (srfi srfi-1))")
+  # scheme_eval(atomspace, """(ure-logger-set-level! "debug")""")
   load_atomspace(datapath, atomspace)
+  if doinhrule:
+    infer_subsets(atomspace)
+  calculate_truth_values(atomspace) # for the memberlink and GO
   gene_singleton_tv(atomspace)
   generate_subsets(atomspace)
   infer_negation(atomspace)
   infer_attractions(atomspace)
-  export_result(datapath, atomspace)
+  get_intentional_similarity(atomspace)
+  return atomspace
+
+def parse_args():
+    parser = argparse.ArgumentParser(description='Generate embedding vector for patients')
+    parser.add_argument('--datapath', type=str, default='',
+                        help='path to the background knowlegde data for gene (e.g membership to GO/Pathway)')
+    parser.add_argument('--outputpath', type=str, default='',
+                        help='path to store the output files')
+    parser.add_argument('--doinhrule', type=bool, default=False,
+                        help='Apply inheritance rule and Propagate the memberlinks relationship to the parent GO')
+    return parser.parse_args()
+
+if __name__=="__main__":
+  arguments = parse_args()
+  if arguments.datapath:
+    base_datasets_dir = arguments.datapath
+  else:
+    base_datasets_dir = os.getcwd() + "/cancer_data/"
+  if arguments.outputpath:
+    base_results_dir = arguments.outputpath
+  else:
+    base_results_dir = os.getcwd() + "/results/cancer-{}/".format(str(date.today()))
+  if arguments.doinhrule:
+    doinhrule = True
+  else:
+    doinhrule = False
+  kbs = generate_links_genes(base_datasets_dir, doinhrule=doinhrule)
+  export_result(base_results_dir, kbs)
+  generate_embeddings(base_results_dir,"GeneNode", kb_atomspace=kbs)
