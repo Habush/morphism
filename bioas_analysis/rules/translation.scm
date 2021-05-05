fcb18eadeaee443878450100ca385c5cfec7e11b
Abdulrahman Semrie <hsamireh@gmail.com>
2021-05-06 00:59:04 +0300

    Add translation and transitivity rules files

---
+++ bioas_analysis/rules/translation.scm
@@ -0,0 +1,22 @@
+(define-module (pln-bio rules translation)
+   #:use-module (opencog) 
+   #:use-module (opencog exec) 
+   #:use-module (opencog ure) 
+   #:use-module (opencog logger)
+   #:use-module (srfi srfi-1)
+   #:use-module (pln-bio rules rule-utils)
+)
+;; Crisp rules about translating a link into another link
+
+;; Helpers
+(define ConceptT (TypeInh "ConceptNode"))
+(define GeneT (Type "GeneNode"))
+
+;; Inheritance to Subset
+(define present-inheritance-to-subset-translation-rule
+  (gen-present-link-translation-rule InheritanceLink SubsetLink ConceptT))
+(define present-inheritance-to-subset-translation-rule-name
+  (DefinedSchemaNode "present-inheritance-to-subset-translation-rule"))
+(DefineLink present-inheritance-to-subset-translation-rule-name
+  present-inheritance-to-subset-translation-rule)
+  
