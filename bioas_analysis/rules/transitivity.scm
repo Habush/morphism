53983820fa786de3d1864a64315e734217f8d128
Abdulrahman Semrie <hsamireh@gmail.com>
2021-05-09 00:06:38 +0300

    Remove parallelism from the translation and transitivity rules

--- bioas_analysis/rules/transitivity.scm
+++ bioas_analysis/rules/transitivity.scm
@@ -5,77 +5,51 @@
 (define ConceptT (TypeInh "ConceptNode"))
 (define GeneT (Type "GeneNode"))
 
-(define-public (gen-present-link-transitivity-rule LINK LINK-TYPE VAR-TYPE)
-  (let* ((X (gar LINK))
-         (Y (gdr LINK))
+(define-public (gen-present-link-transitivity-rule LINK-TYPE VAR-TYPE)
+  (let* ((X (Variable "$X"))
+         (Y (Variable "$Y"))
          (Z (Variable "$Z"))
          (YZ (LINK-TYPE Y Z))
          (XZ (LINK-TYPE X Z)))
     (Bind
       (VariableList
+        (TypedVariable X VAR-TYPE)
+        (TypedVariable Y VAR-TYPE)
         (TypedVariable Z VAR-TYPE))
       (And
         (Present
-          LINK
+          XY
           YZ)
         (Not (Identical X Z)))
       XZ)))
 
-(define-public (gen-present-mixed-link-transitivity-rule LINK-1 LINK-TYPE-1 LINK-TYPE-2
-                                                 Z-TYPE)
-  (let* ((X (gar LINK-1))
-         (Y (gdr LINK-1))
+(define-public (gen-present-mixed-link-transitivity-rule LINK-TYPE-1 LINK-TYPE-2
+                                                  X-TYPE Y-TYPE Z-TYPE)
+  (let* ((X (Variable "$X"))
+         (Y (Variable "$Y"))
          (Z (Variable "$Z"))
+         (XY (LINK-TYPE-1 X Y))
          (YZ (LINK-TYPE-2 Y Z))
          (XZ (LINK-TYPE-1 X Z)))
     (Bind
       (VariableList
+        (TypedVariable X X-TYPE)
+        (TypedVariable Y Y-TYPE)
         (TypedVariable Z Z-TYPE))
       (And
         (Present
-          LINK-1
+          XY
           YZ)
         (Not (Identical X Z)))
       XZ)))
 
 (define-public (gen-present-link-transitivity)
     (cog-logger-info "Running gen-present-link-transitivity")
-    (let* ((atoms (cog-get-atoms 'SubsetLink))
-            (batch-num 0)
-            (batch-size (round (/ (length atoms) (current-processor-count))))
-            (batch-ls (split-lst atoms batch-size))
-            (batches (map (lambda (b) (set! batch-num (+ batch-num 1)) (cons batch-num b)) batch-ls)))
-        
-        (n-par-for-each (current-processor-count)  (lambda (batch)
-              (for-each (lambda (ln)
-                  (gen-present-link-transitivity-rule ln SubsetLink ConceptT)) (cdr batch))) batches)
-        (cog-logger-info "Done!")))
+    (gen-present-link-transitivity-rule SubsetLink ConceptT)
+    (cog-logger-info "Done!")))
 
 (define-public (gen-present-mixed-link-transitivity)
-    ;; get patient atoms and run the deduction in batch
-    (cog-logger-info "Running gen-present-link-transitivity")
-    ;;apply fc to get the relationship between go's and patients
-    (let* ((atoms (cog-get-atoms 'MemberLink))
-            (batch-num 0)
-            (batch-size (round (/ (length atoms) (current-processor-count))))
-            (batch-ls (split-lst atoms batch-size))
-            (batches (map (lambda (b) (set! batch-num (+ batch-num 1)) (cons batch-num b)) batch-ls)))
-        
-        (n-par-for-each (current-processor-count)  (lambda (batch)
-              (for-each (lambda (ln)
-                  (gen-present-mixed-link-transitivity-rule ln MemberLink SubsetLink
-                    ConceptT)) (cdr batch))) batches)
-        (cog-logger-info "Done!")))
-
-(define-public (take-custom lst n)
-    (if (< (length lst) n)
-        (take lst (length lst))
-        (take lst n)))
-
-(define-public (drop-custom lst n)
-    (if (< (length lst) n)
-        (drop lst (length lst))
-        (drop lst n)))
-(define-public (split-lst lst n)
-    (if (null? lst) '()
-        (cons (take-custom lst n) (split-lst (drop-custom lst n) n))))
+    (gen-present-mixed-link-transitivity-rule MemberLink
+                                            SubsetLink
+                                            GeneT ConceptT ConceptT)
+    (cog-logger-info "Done!"))
--- bioas_analysis/rules/translation.scm
+++ bioas_analysis/rules/translation.scm
@@ -4,40 +4,21 @@
 (define ConceptT (TypeInh "ConceptNode"))
 (define GeneT (Type "GeneNode"))
 
-(define-public (gen-present-link-translation-rule LINK-1 LINK-TYPE-2 VAR-TYPE)
-  (let* ((X (gar LINK-1))
-         (Y (gdr LINK-1))
+(define-public (gen-present-link-translation-rule LINK-TYPE-1 LINK-TYPE-2 VAR-TYPE)
+  (let* ((X (Variable "$X"))
+         (Y (Variable "$Y"))
+         (XY-1 (LINK-TYPE-1 X Y))
          (XY-2 (LINK-TYPE-2 X Y)))
     (Bind
+      (VariableList 
+        (TypedVariable X VAR-TYPE)
+        (TypedVariable Y VAR-TYPE))
       (Present
-        LINK-1)
+        XY-1)
       XY-2)))
 
 (define-public (inheritance->subset)
-    ;; get patient atoms and run the deduction in batch
     (cog-logger-info "Inheritance->Subset")
-    ;;apply fc to get the relationship between go's and patients
-    (let* ((atoms (cog-get-atoms 'InheritanceLink))
-            (batch-num 0)
-            (batch-size (round (/ (length atoms) (current-processor-count))))
-            (batch-ls (split-lst atoms batch-size))
-            (batches (map (lambda (b) (set! batch-num (+ batch-num 1)) (cons batch-num b)) batch-ls)))
-        
-        (n-par-for-each (current-processor-count)  (lambda (batch)
-              (for-each (lambda (ln)
-                  (gen-present-link-translation-rule ln SubsetLink ConceptT)) (cdr batch))) batches)
-        (cog-logger-info "Done!")))
-
-(define-public (take-custom lst n)
-    (if (< (length lst) n)
-        (take lst (length lst))
-        (take lst n)))
-
-(define-public (drop-custom lst n)
-    (if (< (length lst) n)
-        (drop lst (length lst))
-        (drop lst n)))
-(define-public (split-lst lst n)
-    (if (null? lst) '()
-        (cons (take-custom lst n) (split-lst (drop-custom lst n) n))))
+    (gen-present-link-translation-rule InheritanceLink SubsetLink ConceptT)
+    (cog-logger-info "Done!"))
   
